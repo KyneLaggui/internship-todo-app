b7036e553419ca9cf31e7dbb1fb35b0c79c2404f
KayDeev/ <92616644+KyneLaggui@users.noreply.github.com>
2024-08-06 09:53:43 +0800

    feat: added sidebar functionality and styling

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import Home from './pages/Home'
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+import Sidebar from './layouts/Sidebar/Sidebar';
 
 function App() {
 
   return (
     <>
+    <Sidebar />
     <Home />
     </>
   )
---
+++ src/layouts/Sidebar/Sidebar.jsx
@@ -0,0 +1,55 @@
+import React, { useState } from 'react';
+import { Button, Nav } from 'react-bootstrap';
+import { FaBars } from 'react-icons/fa';
+import { BsFillHouseFill } from 'react-icons/bs';
+import "./Sidebar.css"
+
+const Sidebar = () => {
+  const [open, setOpen] = useState(false);
+
+  const toggleSidebar = () => {
+    setOpen(!open);
+  };
+
+  return (
+    <div>
+      <div className="d-lg-none">
+        <Button
+          onClick={toggleSidebar}
+          aria-controls="sidebar-content"
+          aria-expanded={open}
+          className="m-2"
+        >
+          <FaBars /> Menu
+        </Button>
+        <div className={`mobile-sidebar bg-dark text-white p-3 ${open ? 'show' : ''}`}>
+          <div className="d-flex justify-content-between align-items-center mb-3">
+            <h4 className="d-flex align-items-center m-0">
+              <BsFillHouseFill className="mr-2" />
+              Motion
+            </h4>
+            <Button variant="light" onClick={toggleSidebar}>Close</Button>
+          </div>
+          <Nav defaultActiveKey="/home" className="flex-column">
+            <Nav.Link href="/home" className="text-white">Home</Nav.Link>
+            <Nav.Link href="/about" className="text-white">About</Nav.Link>
+            <Nav.Link href="/contact" className="text-white">Contact</Nav.Link>
+          </Nav>
+        </div>
+      </div>
+      <div className="d-none d-lg-block bg-dark text-white p-3" id="sidebar">
+        <h4 className="d-flex align-items-center">
+          <BsFillHouseFill className="mr-2" />
+          Motion
+        </h4>
+        <Nav defaultActiveKey="/home" className="flex-column">
+          <Nav.Link href="/home" className="text-white">Home</Nav.Link>
+          <Nav.Link href="/about" className="text-white">About</Nav.Link>
+          <Nav.Link href="/contact" className="text-white">Contact</Nav.Link>
+        </Nav>
+      </div>
+    </div>
+  );
+};
+
+export default Sidebar;
